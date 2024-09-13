27fb0db1885a4b79477a5689a3f128657c52682d
Клим Кузнецов <klimman2000@gmail.com>
2024-09-13 17:15:51 +0200

    Continue

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -5,20 +5,22 @@ from app.schemas.user import UserCreate, UserRead, UserUpdate
 
 router = APIRouter()
 
-router.include_router(
-    # В роутер аутентификации
-    # передается объект бэкенда аутентификации.
-    fastapi_users.get_auth_router(auth_backend),
-    prefix='/auth/jwt',
-    tags=['auth'],
-)
+# Сохраняем роутер в переменную.
+users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
+# Из списка эндпоинтов роутера исключаем ненужную ручку.
+users_router.routes = [
+    rout for rout in users_router.routes if rout.name != 'users:delete_user'
+]
+# Подключаем изменённый роутер по старому адресу.
+
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix='/auth',
     tags=['auth'],
 )
+
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    users_router,
     prefix='/users',
     tags=['users'],
 )
